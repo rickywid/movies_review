f60f7ccf70ca2f925d92849d77b1ebfa46185553
Ricky JW <rickywid@hotmail.com>
2015-09-15 00:17:28 -0400

    first commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,47 @@
+class MoviesController < ApplicationController
+
+	def index
+		@movie = Movie.all
+	end
+
+	def new
+		@movie = current_user.movies.build
+	end
+
+	def create
+		@movie = current_user.movies.build(movie_params)
+		if @movie.save
+			flash[:success] = "Movie Added"
+			redirect_to @movie
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		@movie.update(movie_params)
+		flash[:success] = "Movie Updated"
+		redirect_to @movie
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:success] = "Movie Deleted"
+		redirect_to root_url
+	end
+
+	private
+		def movie_params
+			params.require(:movie).permit(:title, :description, :image)
+		end
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ActiveRecord::Base
+
+	belongs_to :user
+
+  	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :title, :description, presence: true
+end
