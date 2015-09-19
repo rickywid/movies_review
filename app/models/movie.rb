d6afbea2656ab8538a55d7b2e1594678dc233867
Ricky JW <rickywid@hotmail.com>
2015-09-19 16:43:08 -0400

    styled

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
 
-  	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "285x400>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	validates :title, :description, presence: true
